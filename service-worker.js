b637634d4a33b1c8f380eee4581aa20ddce1aae5
arenadointer-afk <arenadointer@gmail.com>
2026-02-25 23:30:39 -0300

    Rename cache from 'Agenda-2' to 'Agenda'

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
   
-const CACHE_NAME = "Agenda-2";
+const CACHE_NAME = "Agenda";
 const ASSETS = [
   "./",
   "./index.html",
