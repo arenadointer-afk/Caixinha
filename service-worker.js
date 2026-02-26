5f518911f26bd09752fc91292cdcf9b50963d2e6
arenadointer-afk <arenadointer@gmail.com>
2026-02-25 22:53:21 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
-const CACHE_NAME = "Agenda-V2";
+const CACHE_NAME = "Agenda-V5";
 const ASSETS = [
 // ... o resto do seu código continua igualzinho para baixo ...
   
