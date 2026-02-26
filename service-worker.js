bc9d00c4289b991af5921007cce699557a2c5910
arenadointer-afk <arenadointer@gmail.com>
2026-02-25 23:28:21 -0300

    Update cache name from Agenda-1 to Agenda-2

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
   
-const CACHE_NAME = "Agenda-1";
+const CACHE_NAME = "Agenda-2";
 const ASSETS = [
   "./",
   "./index.html",
