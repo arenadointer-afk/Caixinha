3068ba142ba7f391192586d098d90080dac2044d
arenadointer-afk <arenadointer@gmail.com>
2026-02-25 23:07:45 -0300

    Update CACHE_NAME from Agenda-V5 to Agenda-V10

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
-const CACHE_NAME = "Agenda-V5";
+const CACHE_NAME = "Agenda-V10";
 const ASSETS = [
 // ... o resto do seu código continua igualzinho para baixo ...
   
